1a987e6095b34daa21be7a282b66cc395550af87
vineetk1 <vineetchandhok@gmail.com>
2017-09-05 11:40:12 -0400

    fixed bugs

--- classes/Model.py
+++ classes/Model.py
@@ -4,7 +4,7 @@ This program comes with ABSOLUTELY NO WARRANTY.
 '''
 import logging
 logger = logging.getLogger()
-import classes.CommonModel
+import classes.CommonModel as cm
 import pathlib
 import keras.models
 import sys
@@ -17,18 +17,18 @@ import contextlib
     #logger.info('h5py cannot be imported')    
     #h5py = None
 
-class Model(classes.CommonModel.CommonModel):
+class Model(cm.CommonModel):
     def __init__(self):
         pass
 
     def _modelInternal(self, _args):
-        if classes.CommonModel.CommonModel.Kmodel is None:
+        if cm.CommonModel.Kmodel is None:
             print('No model file available. Must load a model file first')
             return
         if _args.outFile is None:
-            if _args.summary:       classes.CommonModel.CommonModel.Kmodel.summary()
-            if _args.configuration: print(classes.CommonModel.CommonModel.Kmodel.get_config())
-            if _args.weights:       print(classes.CommonModel.CommonModel.Kmodel.get_weights())
+            if _args.summary:       cm.CommonModel.Kmodel.summary()
+            if _args.configuration: print(cm.CommonModel.Kmodel.get_config())
+            if _args.weights:       print(cm.CommonModel.Kmodel.get_weights())
         else:
             try:
                 logger.debug('_args.outFile = {}'.format(_args.outFile))
@@ -41,9 +41,9 @@ class Model(classes.CommonModel.CommonModel):
                 return
             with _absOutFile.open('w') as _outfile:
                 with contextlib.redirect_stdout(_outfile):
-                    if _args.summary:       classes.CommonModel.CommonModel.Kmodel.summary()
-                    if _args.configuration: print(classes.commonmodel.commonmodel.kmodel.get_config())
-                    if _args.weights:       print(classes.commonmodel.commonmodel.kmodel.get_weights())
+                    if _args.summary:       cm.CommonModel.Kmodel.summary()
+                    if _args.configuration: print(cm.CommonModel.Kmodel.get_config())
+                    if _args.weights:       print(cm.CommonModel.Kmodel.get_weights())
 
     def settingsload(self, _settings):
         pass
@@ -59,21 +59,21 @@ class Model(classes.CommonModel.CommonModel):
 
     def execute(self, _line):
         logger.debug('_line = {}, shlex.split(_line) = {}'.format(_line, shlex.split(_line)))
-        _modelp = argparse.argumentparser(prog="model", 
+        _modelP = argparse.ArgumentParser(prog="model", 
             description='information on the model',
             epilog='long options can be abbreviated if they are unambiguous in the commandline') 
-        _modelp.add_argument('--summary', '-s', action='store_true', 
+        _modelP.add_argument('--summary', '-s', action='store_true', 
                 help='show the summary of the model')
-        _modelp.add_argument('--configuration', '-c', action='store_true', 
+        _modelP.add_argument('--configuration', '-c', action='store_true', 
                 help='show the configuration of the model')
-        _modelp.add_argument('--weights', '-w', action='store_true', 
+        _modelP.add_argument('--weights', '-w', action='store_true', 
                 help='show the weights of the model')
-        _modelp.add_argument('--outfile', '-f', action='store', metavar='filename',
+        _modelP.add_argument('--outFile', '-f', action='store', metavar='fileName',
                 help='path of the file where the output will be written')
         try:
-            _args = _modelp.parse_args(shlex.split(_line))
+            _args = _modelP.parse_args(shlex.split(_line))
             logger.debug('{}'.format(_args))
-        except systemexit: # normal exit
+        except SystemExit: # normal exit
             return
         except:
             print("Unexpected error: {}".format(sys.exc_info()[0]))
