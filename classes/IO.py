175a245988bb3a0576be7856ec2f7ab2f7bf1d16
vineetk1 <vineetchandhok@gmail.com>
2017-09-21 16:08:56 -0400

    added more functionality

--- classes/IO.py
+++ classes/IO.py
@@ -21,77 +21,104 @@ class IO(cm.CommonModel):
 
     def _listLayersNumbersNames(self, _args):
         # output to stdout a list of layer numbers and names of the model
-        for layerNum, layer in enumerate(cm.CommonModel.Kmodel.layers): 
-            print('layer {}: {:17}'.format(layerNum, layer.get_config()['name']), end="")
+        for _layerNum, _layer in enumerate(cm.CommonModel.Kmodel.layers): 
+            print('layer {}: {:17}'.format(_layerNum, _layer.get_config()['name']), end="")
         print('\n')
 
-    def numpyLoadFile(self, _file):
+    def _lastLayerNum(self):
+        # convenience method to get the layer number of the last layer in the model
+        try:
+            # is there a better method of getting the number of the last layer?
+            for _layerNum, _layer in enumerate(cm.CommonModel.Kmodel.layers): pass
+            return _layerNum
+        except: return None # cm.CommonModel.Kmodel is None
+
+    def _numpyLoadFile(self, _file):
         # convenience method to deserializes a numpy file _file
-        try: deserializedFile = numpy.load(_file)
+        try: _deserializedFile = numpy.load(_file)
         except (ValueError, OSError) as err:
             print('Invalid {}\n{}'.format(_file, err))
             raise
         except: print("Unexpected error: {}".format(sys.exc_info()[0])); raise
-        return deserializedFile
+        return _deserializedFile
 
     def _setup(self, _args):
-        # setup the parameters to enable the model to be run
+        # setup the parameters to enable the model to run
         self._inputLayerNumber = _args.inputLayerNumber
+        assert(self._inputLayerNumber is None)
         self._outputLayerNumbers = _args.outputLayerNumbers
         if self._outputLayerNumbers is None:
-            # get the number of the last layer; is there a better way of getting this number?
-            for layerNum, layer in enumerate(cm.CommonModel.Kmodel.layers): 
-                outputLastLayerNum = layerNum
-            self._outputLayerNumbers = [outputLastLayerNum] 
+            _lastLayerNum = self._lastLayerNum()
+            if _lastLayerNum is not None: self._outputLayerNumbers = [_lastLayerNum]
         try: 
             self._inFile = super().absPathFile(_args.inFile)
-            self._input = self.numpyLoadFile(self._inFile)
-            print('input file: type = {}, shape = {}, dtype = {}'.format(
-                        type(self._input), self._input.shape, self._input.dtype))
-        except: 
-            self._inFile = None
-            self._input = None
+            self._inputData = self._numpyLoadFile(self._inFile)
+        except: self._inFile = None; self._inputData = None
         try: 
             self._exptdOutFile = super().absPathFile(_args.exptdOutFile)
-            self._exptdOutput = self.numpyLoadFile(self._exptdOutFile)
-            print('expected output file: type = {}, shape = {}, dtype = {}\n{}'.format(
-                        type(self._exptdOutput), self._exptdOutput.shape, 
-                        self._exptdOutput.dtype, self._exptdOutput[0:1]))
-        except: 
-            self._exptdOutFile = None
-            self._exptdOutput = None
+            self._exptdOutput = self._numpyLoadFile(self._exptdOutFile)
+        except: self._exptdOutFile = None; self._exptdOutput = None
         try: self._outFile = super().absPathFile(_args.outFile)
         except: self._outFile = None
 
     def _run(self, _args):
         # run the model to produce outputs for a given input
         if cm.CommonModel.Kmodel is None:
-            print('No model file available. Must load a model file first')
+            print('No model file available. Must load a model file first. Cannot force run using \"run -f\"')
             return
-        if self._input is None:
-            print('No input file available. Must specify an input file first')
-            return
-        if self._outFile is None:
-            for layerNum, layer in enumerate(cm.CommonModel.Kmodel.layers): 
-                if layerNum in _args.outputLayerNumbers:
-                    referenceToClass_tensorflowBackendFunction = keras.backend.function([cm.CommonModel.Kmodel.layers[0].input], [cm.CommonModel.Kmodel.layers[layerNum].output])
-                    layerOutput = referenceToClass_tensorflowBackendFunction([self._input[0:1]])[0]
-                    print('\nlayer {}: name = {}, type = {}, shape = {}, dtype = {}\n{}'.format(
+        if self._inputData is None:
+            print('No input file available. Must load an input file first. Cannot force run using \"run -f\"')
+            shouldReturn = True
+        assert(self._inputLayerNumber is not None)
+        _lastLayerNum = self._lastLayerNum()
+        if (self._inputLayerNumber < 0) or (self._inputLayerNumber > _lastLayerNum):
+            print("Input layer {} does not exist. Using default layer 0 instead".format(
+                self._inputLayerNumber))
+            self._inputLayerNumber = 0
+        if self._outputLayerNumbers is None:
+            self._outputLayerNumbers = [_lastLayerNum]
+            print("No ouput layer specified. Using default last layer {}".format(self._outputLayerNumbers))
+
+        shouldReturn = False
+        if self._inputData.shape != cm.CommonModel.Kmodel.layers[self._inputLayerNumber].input_shape:
+                print('Error: Shape {} of data in input-file MUST be equal to shape {} of input in layer {}'.format(
+                self._inputData.shape,  cm.CommonModel.Kmodel.layers[self._inputLayerNumber].input_shape, 
+                self._inputLayerNumber))
+                shouldReturn = True
+        if (self._exptdOutput is not None) and (len(self._outputLayerNumbers) != 1):
+            print('Error: The expected-output-file MUST be associated with only one output-layer {}'.format(
+                self._outputLayerNumbers))
+            shouldReturn = True
+        if shouldReturn and (not _args.force): return
+
+        for layerNum, layer in enumerate(cm.CommonModel.Kmodel.layers): 
+            if layerNum in self._outputLayerNumbers:
+                referenceToClass_tensorflowBackendFunction = keras.backend.function(
+                        [cm.CommonModel.Kmodel.layers[self._inputLayerNumber].input], [layer.output])
+                layerOutput = referenceToClass_tensorflowBackendFunction([self._inputData[0:1]])[0]
+                print('\nlayer {}: name = {}, type = {}, shape = {}, dtype = {}\n{}'.format(
                         layerNum, layer.get_config()['name'], type(layerOutput), layerOutput.shape, 
                         layerOutput.dtype, layerOutput))
 
+
     def settingsLoad(self, _settings):
-        # upon start of this interactive program, load previously saved settings in _settings
+        # upon start of this interactive program, load previously saved settings from _settings
         try:
-            self._inputLayerNumber = _settings['ioInputLayerNumber']
-            self._outputLayerNumbers = _settings['ioOutputLayerNumbers']
-            self._inFile = _settings['ioInFile']
-            self._exptdOutFile = _settings['ioExptdOutFile']
-            self._outFile = _settings['ioOutFile']
+            _inputLayerNumber = _settings['ioInputLayerNumber']
+            _outputLayerNumbers = _settings['ioOutputLayerNumbers']
+            _inFile = _settings['ioInFile']
+            _exptdOutFile = _settings['ioExptdOutFile']
+            _outFile = _settings['ioOutFile']
         except:
-            logger.warn('one or more IO files  not in settings = {}; settings file possibly corrupted'.format(
+            logger.warn('one or more items not in settings = {}; settings file possibly corrupted'.format(
                 _settings))
             raise
+        _args = argparse.Namespace(inputLayerNumber=locals()["_inputLayerNumber"], 
+                outputLayerNumbers=locals()["_outputLayerNumbers"], inFile=locals()["_inFile"], 
+                exptdOutFile=locals()["_exptdOutFile"], outFile=locals()["_outFile"])
+        logger.debug(_args)
+        # Following statement is not in a try-except block because exceptions are handled by the called method
+        self._setup(_args)
 
     def settingsSave(self, _settings):
         # save the settings in _settings before exiting this interactive program
@@ -103,8 +130,8 @@ class IO(cm.CommonModel):
 
     def settingsDefault(self):
         # reset settings to their default state
-        self._inputLayerNumber = None
-        self._outputLayerNumbers = None
+        self._inputLayerNumber = 0 
+        self._outputLayerNumbers = None # None because cm.CommonModel.Kmodel is None
         self._inFile = None
         self._exptdOutFile = None
         self._outFile = None
@@ -113,9 +140,9 @@ class IO(cm.CommonModel):
         # print the settings at the stdout
         print('io input layer number: {}'.format(self._inputLayerNumber))
         print('io output layer numbers: {}'.format(self._outputLayerNumbers))
-        print('io input file: {}'.format(self._inFile if self._inFile else None))
-        print('io expected output file: {}'.format(self._exptdOutFile if self._exptdOutFile else None))
-        print('io output file: {}'.format(self._outFile if self._outFile else None))
+        print('io input file: {}'.format(self._inFile))
+        print('io expected output file: {}'.format(self._exptdOutFile))
+        print('io output file: {}'.format(self._outFile))
 
     def execute(self, _line):
         # execute user input
@@ -131,21 +158,21 @@ class IO(cm.CommonModel):
 
         _ioPsSetup = _ioPs.add_parser('setup', aliases=['se'],
                 help='setup the model before running it')
-        #_ioPsSetup.add_argument('--inFile', '-if', action='store', metavar='fileName',
-        #_ioPsSetup.add_argument('inFile', action='store', metavar='fileName',
         _ioPsSetup.add_argument('inFile', action='store', 
-                help='path of the file that has the input to the model; the file must be a numpy array')
+                help='path of the file that has the input to the model; the file suffix must be ".npy" and it  must be a numpy array')
         _ioPsSetup.add_argument('--inputLayerNumber', '-il', type=int, action='store', default = 0,
                 help='input will be applied to the specified layer number; e.g. \"-il 5\" means that the input will be applied to layer 5; default includes the first layer, i.e. layer 0')
         _ioPsSetup.add_argument('--outputLayerNumbers', '-ol', type=int, nargs='+',
                 help='outputs will be retrieved from the specified layer numbers; e.g. \"-ol 3 8 9\" means that the outputs will be retrieved from layers 3, 8, and 9; default includes only the last layer')
         _ioPsSetup.add_argument('--exptdOutFile', '-ef', action='store', metavar='fileName',
-                help='path of the file that has the expected output of the last layer only; the file must be a numpy array')
+                help='path of the file that has the expected output of the last layer only;  the file suffix must be ".npy" and it  must be a numpy array')
         _ioPsSetup.add_argument('--outFile', '-of', action='store', metavar='fileName',
                 help='path of the file where the output will be written')
         _ioPsSetup.set_defaults(func=self._setup)
 
         _ioPsRun = _ioPs.add_parser('run', help='run the model')
+        _ioPsRun.add_argument('--force', '-f', action='store_true', 
+                help='force the model to run even when errors are flagged')
         _ioPsRun.set_defaults(func=self._run)
 
         try:
